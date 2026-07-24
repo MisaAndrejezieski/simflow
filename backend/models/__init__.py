6a1558ee5c13008cf1d9c107b48da461afbf414e
MisaAndrejezieski <165926051+MisaAndrejezieski@users.noreply.github.com>
2026-07-24 14:56:23 -0300

    Update __init__.py

---
+++ backend/models/__init__.py
@@ -0,0 +1,7 @@
+from models.etiqueta import Etiqueta
+from models.melhoria import Melhoria
+from models.quase_acidente import QuaseAcidente
+from models.reuniao_ga import ReuniaoGA
+from models.usuario import Usuario
+
+__all__ = ['Usuario', 'Etiqueta', 'ReuniaoGA', 'Melhoria', 'QuaseAcidente']
